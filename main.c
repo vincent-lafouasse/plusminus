b730adb29b62aa444c45fc5afd1dbf59a63819ad
vincent-lafouasse <vincentlafouasse@live.fr>
2021-12-17 00:14:33 +0100

    format using `format-clang`

--- main.c
+++ main.c
@@ -1,85 +1,79 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 
-int		difficulty_choice(void);
-void		plusminus (int max);
+int difficulty_choice(void);
+void plusminus(int max);
 
-int
-main(void)
-{
-	int		play_again = 1;
-	srand(time(NULL));
+int main(void) {
+  int play_again = 1;
+  srand(time(NULL));
 
-	printf(" == Welcome to plusminus ! == \n\n");
+  printf(" == Welcome to plusminus ! == \n\n");
 
-	do {
-		plusminus(difficulty_choice());
-		printf("\nPress 1 to play again, otherwhise press 0\n");
-		scanf("%d", &play_again);
-		printf("\n");
-	} while (play_again);
+  do {
+    plusminus(difficulty_choice());
+    printf("\nPress 1 to play again, otherwhise press 0\n");
+    scanf("%d", &play_again);
+    printf("\n");
+  } while (play_again);
 
-	printf("Thank you !\n");
-	return 0;
+  printf("Thank you !\n");
+  return 0;
 }
 
-void
-plusminus(int max)
-{
-	const int	MIN = 1;
-	int		mystery_number = 0;
-	int		guess = 0;
-	int		counter = 0;
+void plusminus(int max) {
+  const int MIN = 1;
+  int mystery_number = 0;
+  int guess = 0;
+  int counter = 0;
 
-	mystery_number = (rand() % (max - MIN + 1)) + MIN;
-	/* Generates a random number between max and MIN */
+  mystery_number = (rand() % (max - MIN + 1)) + MIN;
+  /* Generates a random number between max and MIN */
 
-	do {
-		scanf("%d", &guess);
+  do {
+    scanf("%d", &guess);
 
-		if (guess < MIN || guess > max) {
-			printf("This number is not valid, please try again.\n");
-		} else if (guess < mystery_number) {
-			printf("(+) The mystery number is greater than that\n\n");
-			counter++;
-		} else if (guess > mystery_number) {
-			printf("(-) The mystery number is lower than that\n\n");
-			counter++;
-		}
-	} while (guess != mystery_number);
-	counter++;
+    if (guess < MIN || guess > max) {
+      printf("This number is not valid, please try again.\n");
+    } else if (guess < mystery_number) {
+      printf("(+) The mystery number is greater than that\n\n");
+      counter++;
+    } else if (guess > mystery_number) {
+      printf("(-) The mystery number is lower than that\n\n");
+      counter++;
+    }
+  } while (guess != mystery_number);
+  counter++;
 
-	printf("\n");
-	printf("Well done, it was indeed %d.\n", mystery_number);
-	printf("And you only took %d turns !\n\n", counter);
+  printf("\n");
+  printf("Well done, it was indeed %d.\n", mystery_number);
+  printf("And you only took %d turns !\n\n", counter);
 }
 
-int
-difficulty_choice(void)
-{
-	int		difficulty = 2;
-	int		max        [] = {1, 50, 100, 1000, 10000};
+int difficulty_choice(void) {
+  int difficulty = 2;
+  int max[] = {1, 50, 100, 1000, 10000};
 
-	printf("What difficulty setting do you choose ?\n");
-	printf("1. chill \n");
-	printf("2. standard \n");
-	printf("3. annoying\n");
-	printf("4. i wouldnt do this\n\n");
+  printf("What difficulty setting do you choose ?\n");
+  printf("1. chill \n");
+  printf("2. standard \n");
+  printf("3. annoying\n");
+  printf("4. i wouldnt do this\n\n");
 
-	scanf("%d", &difficulty);
-	printf("\n");
+  scanf("%d", &difficulty);
+  printf("\n");
 
-	if (difficulty == 0) {
-		printf("You have unlocked the hidden difficulty setting.\n");
-		printf("Be prepared for an exciting game.\n\n");
-	}
-	printf("To win at plusminus, you will have to guess a number.\n");
-	printf("This number has been randomly selected between 1 and %d.\n\n", max[difficulty]);
-	printf("Good luck !\n\n");
-	printf("What's your guess ?\n\n");
+  if (difficulty == 0) {
+    printf("You have unlocked the hidden difficulty setting.\n");
+    printf("Be prepared for an exciting game.\n\n");
+  }
+  printf("To win at plusminus, you will have to guess a number.\n");
+  printf("This number has been randomly selected between 1 and %d.\n\n",
+         max[difficulty]);
+  printf("Good luck !\n\n");
+  printf("What's your guess ?\n\n");
 
-	return max[difficulty];
-
+  return max[difficulty];
 }
